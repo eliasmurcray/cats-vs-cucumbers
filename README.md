bcc37806c8bcb6be9de0ea5389fb2efe85ad2fe3
eliasmurcray <eliasmurcray@gmail.com>
2023-02-07 10:47:26 -0800

    Add player movements

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cats Vs Cucumbers
+The official Cats vs Cucumbers repository. Any questions can be asked on [Discord](https://discord.com/invite/peexFK5dz6)
+
+
+## Contributing
+1. `git clone https://github.com/eliasmurcray/cats-vs-cucumbers.git`
+2. `npm install` in Cats vs Cucumbers directory
+3. Make your changes
+3. `npm run build`
+4. Use the Live Server extension to preview the program
--- ts/index.ts
+++ ts/index.ts
@@ -10,10 +10,64 @@ const ctx = canvas.getContext("2d");
 const keys = {};
 setupKeys(canvas, keys);
 
-function animationLoop() {
-  requestAnimationFrame(animationLoop);
+class Player {
 
+  xVel: number;
+  yVel: number;
+  walkSpeed: number;
+  isJumping: boolean;
+
+  constructor(public x: number, public y: number) {
+    this.xVel = 0;
+    this.yVel = 0;
+    this.walkSpeed = 2;
+    this.isJumping = false;
+  }
+
+  render(ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = "#000";
+    ctx.beginPath();
+    ctx.ellipse(this.x, this.y, 15, 15, 0, 0, Math.PI * 2);
+    ctx.fill();
+  }
+
+  update() {
+    this.xVel = 0;
+    this.yVel = 0;
+    if(keys["arrowup"])
+      this.yVel -= this.walkSpeed;
+    if(keys["arrowdown"])
+      this.yVel += this.walkSpeed;
+    if(keys["arrowright"])
+      this.xVel += this.walkSpeed
+    if(keys["arrowleft"])
+      this.xVel -= this.walkSpeed
+    this.x += this.xVel;
+
+    this.y += this.yVel;
+
+  }
+};
+
+const player = new Player(30, 30);
+
+const FPS = 60;
+const FRAME_TIME = 1000 / FPS;
+function animationLoop(then: number) {
+  const now = performance.now();
+  const elapsed = now - then;
+  if(elapsed > FRAME_TIME) {
+    then = now - (elapsed % FRAME_TIME);
+    // Execute game code here
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    player.render(ctx);
+    player.update();
+  }
+  requestAnimationFrame(() => animationLoop(then));
 }
+
+animationLoop(performance.now());
 
 function setupKeys(canvas: HTMLCanvasElement, keys: Keys) {
   canvas.tabIndex = -1;
